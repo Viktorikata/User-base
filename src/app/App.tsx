11c28671eb25274a5164ef1ae2076b0906ee3b01
Viktoria <va.2816275@yandex.com>
2026-01-27 16:19:37 +0300

    Init project: webpack + react + ts

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "antd/dist/reset.css";
+import { Button } from "antd";
+
+export function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>User Base (starter)</h1>
+      <Button type="primary">AntD работает</Button>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isDev = argv.mode !== "production";
+
+  return {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.[contenthash].js",
+      clean: true,
+      publicPath: "/",
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+        test: /\.tsx?$/,
+        use: {
+            loader: "ts-loader",
+            options: {
+            transpileOnly: true,
+            configFile: path.resolve(__dirname, "tsconfig.json"),
+            },
+        },
+        exclude: /node_modules/,
+        },
+        {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        },        
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    devtool: isDev ? "eval-source-map" : "source-map",
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      open: true,
+      hot: true,
+    },
+  };
+};
